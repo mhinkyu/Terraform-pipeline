14a365da7a20c6e6b8f628f199ad60db48ec41bf
Mhinkyu <mhinkyuk@gmail.com>
2022-11-01 17:14:04 +0200

    ssh port ip address updated

--- network.tf
+++ network.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "ssh-allowed" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["77.137.68.247/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
